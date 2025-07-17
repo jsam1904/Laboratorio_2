938aac3cf31bec32638f98171147ddf66399b547
jsam1904 <javierse.alvarado@gmail.com>
2025-07-17 08:48:14 -0600

    En una lista y despues se muestra los numeros impares

--- src/Main.kt
+++ src/Main.kt
@@ -7,7 +7,17 @@ fun calcularPromedio (numeros: List<Double>): Double {
 }
 
 fun main() {
+    println("----------Promedio----------")
     val lista = listOf(1.1, 2.2, 3.3, 4.0, 5.6, 8.5, 10.5)
     val promedio = calcularPromedio(lista)
     println("El promedio es:  $promedio")
+
+    println("")
+    println("----------Filtro----------")
+    val numero = listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+    val impares = numero.filter { numero -> numero % 2 == 1 }
+    println("El impares es: $impares")
+
+    println("")
+    println("----------Filtro----------")
 }

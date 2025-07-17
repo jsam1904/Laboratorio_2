b344f6fadd24b2a579064bfbd068f7f25b8f0469
jsam1904 <javierse.alvarado@gmail.com>
2025-07-17 08:41:42 -0600

    Se calcula el promedio de la samua de numeros en una lista

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun calcularPromedio (numeros: List<Double>): Double {
+    if (numeros.isEmpty()) {
+        return 0.0
     }
+    val suma = numeros.reduce { acumulado, numero -> acumulado + numero }
+    return suma / numeros.size
+}
+
+fun main() {
+    val lista = listOf(1.1, 2.2, 3.3, 4.0, 5.6, 8.5, 10.5)
+    val promedio = calcularPromedio(lista)
+    println("El promedio es:  $promedio")
 }
